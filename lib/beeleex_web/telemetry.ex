6104167a571434eeffd0927ad7e6dc6f8d1fa8a5
David El-Ghoul <daveelghoul@gmail.com>
2022-12-06 09:20:18 +0200

    PR changes

--- lib/beeleex_web/telemetry.ex
+++ lib/beeleex_web/telemetry.ex
@@ -28,13 +28,7 @@ defmodule BeeleexWeb.Telemetry do
       summary("phoenix.router_dispatch.stop.duration",
         tags: [:route],
         unit: {:native, :millisecond}
-      ),
-
-      # VM Metrics
-      summary("vm.memory.total", unit: {:byte, :kilobyte}),
-      summary("vm.total_run_queue_lengths.total"),
-      summary("vm.total_run_queue_lengths.cpu"),
-      summary("vm.total_run_queue_lengths.io")
+      )
     ]
   end
 
